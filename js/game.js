3c6a6790ae786738089582391fa570e57792a218
Pedro Alberto Baños Folcra <pedroalbertobanosfolcra@MacBook-Pro-de-Pedro-2.local>
2021-12-16 22:39:13 +0100

    almost done cannot print the bullet and move it

--- js/game.js
+++ js/game.js
@@ -8,12 +8,14 @@ class Game {
         this.arena = new Arena(ctx)
         this.player = new Player(ctx, 400, 350)
         this.zombies = []
+        this.bullet = []
 
         this.intervalId = undefined
         this.fps = 1000 / 60
 
         this.obstaclesFramesCount = 0;
         this.tick = 0;
+        this.score = 0 
 
     }
 
@@ -55,22 +57,42 @@ class Game {
         this.arena.draw()
         this.player.draw()
         
+        
         this.zombies.forEach(zombie => zombie.draw())
+
+       // this.bullet.forEach(bullet =>bullet.draw())//revisar
+        
+        this.drawScore()
+
         
     }
+
+    drawScore() {
+        this.ctx.save()
+    
+        this.ctx.fillStyle = 'black'
+        this.ctx.font = ' bold 24px sans-serif'
+    
+        this.ctx.fillText(`Score: ${this.score} ptos`, 20, 40)
+    
+        this.ctx.restore()
+      }
 
 
     move(){
         this.zombies.forEach(zombie => zombie.move())
+        //this.bullet.forEach(bullet => bullet.move()) //revisar
         this.player.move()
 
     }
     onKeyUp(keyCode) {
         this.player.onKeyUp(keyCode)
+        //this.bullet.onKeyUp(keyCode) //revisar
 
     }
     onKeyDown(keyCode) {
         this.player.onKeyDown(keyCode)
+        //this.bullet.onKeyDown(keyCode) // revisar
     
     }
 
@@ -88,28 +110,36 @@ class Game {
     checkCollission() {
         
         const condition = this.zombies.some(zombie => this.player.collidesWith(zombie))
-
+        //const conditionTwo = this.bullets.some(bullet => this.zombies.collidesWith(bullet))
         if (condition) {
+            
             this.gameOver()
           }
+          /*if(conditionTwo){ //revisar
+
+            this.zombies = this.zombies.filter(zombie => zombie !== conditionTwo) //revisar
+
+            this.score++ //revisar
+          }*/
+
         }
 
       
-    gameOver() {
-          clearInterval(this.intervalId)
-      
-          this.ctx.save()
-          
-          this.ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'
-          this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
-      
-          this.ctx.fillStyle = 'white'
-          this.ctx.textAlign = 'center'
-          this.ctx.font = 'bold 32px sans-serif'
-          this.ctx.fillText('Game Over', this.ctx.canvas.width / 2, this.ctx.canvas.height / 2)
-      
-          this.ctx.restore()
-    }
+        gameOver() {
+            clearInterval(this.intervalId)
+        
+            this.ctx.save()
+            
+            this.ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'
+            this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+        
+            this.ctx.fillStyle = 'white'
+            this.ctx.textAlign = 'center'
+            this.ctx.font = 'bold 32px sans-serif'
+            this.ctx.fillText('Game Over', this.ctx.canvas.width / 2, this.ctx.canvas.height / 2)
+        
+            this.ctx.restore()
+          }
     
 }
 
